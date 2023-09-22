91c4da6f98d4f951709672ea02604c118ebf7488
Jharsh17 <76389694+Jharsh17@users.noreply.github.com>
2023-09-22 22:05:26 +0530

    Add files via upload

---
+++ src/Components/NotStarted.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NotStarted = (props) => {
+    return (
+        <div className="login-container">
+            <h1 className="welcome-message">Voting is Not Started</h1>
+        </div>
+    )
+}
+
+export default NotStarted;
