edd2e2a6f305f866c92ffb7b71bb229e6db05c8f
Jharsh17 <76389694+Jharsh17@users.noreply.github.com>
2023-09-22 22:02:42 +0530

    Add files via upload

---
+++ contracts/Voting.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.11;
+import "hardhat/console.sol";
+
+contract Voting {
+    struct Candidate {
+        string name;
+        uint256 voteCount;
+    }
+
+    Candidate[] public candidates;
+    address owner;
+    mapping(address => bool) public voters;
+
+    uint256 public votingStart;
+    uint256 public votingEnd;
+
+constructor(string[] memory _candidateNames, uint256 _durationInMinutes, uint256 _customVotingStart) {
+    for (uint256 i = 0; i < _candidateNames.length; i++) {
+        candidates.push(Candidate({
+            name: _candidateNames[i],
+            voteCount: 0
+        }));
+    }
+    owner = msg.sender;
+    require(_customVotingStart >= block.timestamp, "Custom start time must be in the future.");
+    votingStart = _customVotingStart;
+    votingEnd = votingStart + (_durationInMinutes * 1 minutes);
+}
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function addCandidate(string memory _name) public onlyOwner {
+        candidates.push(Candidate({
+                name: _name,
+                voteCount: 0
+        }));
+    }
+
+    function vote(uint256 _candidateIndex) public {
+        require(!voters[msg.sender], "You have already voted.");
+        require(_candidateIndex < candidates.length, "Invalid candidate index.");
+
+        candidates[_candidateIndex].voteCount++;
+        voters[msg.sender] = true;
+    }
+
+    function getAllVotesOfCandiates() public view returns (Candidate[] memory){
+        return candidates;
+    }
+
+    function getVotingStatus() public view returns (bool) {
+        return (block.timestamp >= votingStart && block.timestamp < votingEnd);
+    }
+
+    function getRemainingTime() public view returns (uint256) {
+        if(block.timestamp < votingStart){
+            return 0;
+        }
+        if (block.timestamp >= votingEnd) {
+            return 1;
+    }
+        return votingEnd - block.timestamp+1;
+    }
+}
